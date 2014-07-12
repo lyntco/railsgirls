29e4100f284b04997e3f94a3695f8d3f90654d7c
lyntco <strawww@gmail.com>
2014-07-12 15:02:17 +1000

    Refactor TDD

--- tdd/roman.rb
+++ tdd/roman.rb
@@ -1,37 +1,19 @@
 def roman(n)
-  thousands = n.divmod(1000).first
-  hundreds = n.divmod(1000).last.divmod(100).first
-  tens, ones = n.divmod(1000).last.divmod(100).last.divmod(10)
-  # thousands = { n.divmod(1000).first => ["M"] }
-  # hundreds = { n.divmod(1000).last.divmod(100).first => ["D","C"] }
-  # tens = { n.divmod(1000).last.divmod(100).last.divmod(10).first => ["L","D"] }
-  # ones = { n.divmod(1000).last.divmod(100).last.divmod(10).last => ["V","I"] }
-  # ones_letters =["V","I"]
-  # first = ["D", "L", "V"]
-  # second = ["C", "X", "I"]
+  thousands = { n.divmod(1000).first => "M" }
+  hundreds = { n.divmod(1000).last.divmod(100).first => ["D","C"] }
+  tens = { n.divmod(1000).last.divmod(100).last.divmod(10).first => ["L","X"] }
+  ones = { n.divmod(1000).last.divmod(100).last.divmod(10).last => ["V","I"] }
   str = ""
 
-  # [thousands,hundreds,tens,ones].each do |magnitude|
-  #   magnitude.each do |n,letters|
-  #     modres = n % 5
-  #     str += letters[0] if n >= 5
-  #     str += letters[-1] * modres
-  #   end
-  # end
+  str += thousands.values.first * thousands.keys.first
 
-  str += "M" * thousands
-
-  modreshuns = hundreds % 5
-    str += "D" if hundreds >= 5
-    str += "C" * modreshuns
-
-  modrestens = tens % 5
-    str += "L" if tens >= 5
-    str += "X" * modrestens
-
-  modresones = ones % 5
-    str += "V" if ones >= 5
-    str += "I" * modresones
+  [hundreds,tens,ones].each do |magnitude|
+    magnitude.each do |amount,letters|
+      modres = amount % 5
+      str += letters[0] if amount >= 5
+      str += letters[-1] * modres
+    end
+  end
 
   str
 end
